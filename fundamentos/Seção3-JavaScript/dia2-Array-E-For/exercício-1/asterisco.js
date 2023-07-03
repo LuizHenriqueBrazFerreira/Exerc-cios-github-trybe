018ee58ac76061bbc99d5bdcca24fc1eac269a1c
Luiz-Henrique <lhenrique78155@gmail.com>
2023-07-03 16:12:39 -0300

    desenvolvendo 3 exercícios

---
+++ fundamentos/Seção3-JavaScript/dia2-Array-E-For/exercício-1/asterisco.js
@@ -0,0 +1,9 @@
+const n = 5;
+let asterisco = '*';
+let linha;
+for(let linhaIndex = 0; linhaIndex < n; linhaIndex +=1){
+  linha = linha + asterisco;
+}
+for(let linhaIndex = 0; linhaIndex < n; linhaIndex +=1){
+  console.log(linha);
+}
---
+++ fundamentos/Seção3-JavaScript/dia2-Array-E-For/exercício-1/piramideAsterisco.js
@@ -0,0 +1,10 @@
+const n = 5;
+let asterisco = '*';
+let linha = '';
+let coluna;
+
+for(let linhaIndex = 0; linhaIndex < n; linhaIndex +=1){
+  linha = linha + asterisco;
+  console.log(linha);
+};
+
